b5d1a6846d78ac7aed67c6a897a9e0a86f4c9403
Thomas Braun <tbraun96@gmail.com>
2018-06-12 17:49:32 -0700

    init

---
+++ Controllers/XyzController.cs
@@ -0,0 +1,49 @@
+﻿using SlimGis.MapKit.Geometries;
+using SlimGis.MapKit.Layers;
+using SlimGis.MapKit.Symbologies;
+using SlimGis.MapKit.Utilities;
+using SlimGis.MapKit.WebApi;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace geovisual.Controllers
+{
+    [RoutePrefix("xyz")]
+    public class XyzController : ApiController
+    {
+        [HttpGet]
+        [Route("{z}/{x}/{y}")]
+        public IHttpActionResult GetMap(int z, int x, int y)
+        {
+            
+            ShapefileLayer layer = new ShapefileLayer(HttpContext.Current.Server.MapPath("~/App_Data/world-bound-900913.shp"));
+            layer.Styles.Add(new FillStyle(GeoColor.FromHtml("#AAFFDF3E"), GeoColors.White));
+
+            ShapefileLayer layer2 = new ShapefileLayer(HttpContext.Current.Server.MapPath("~/App_Data/user_timelines_since_t1_w_coordinates.shp"));
+            layer2.Styles.Add(new FillStyle(GeoColor.FromHtml("#AAFFDF3E"), GeoColors.Red));
+
+            MapModel mapModel = new MapModel(GeoUnit.Meter);
+            mapModel.Layers.Add(layer);
+            mapModel.Layers.Add(layer2);
+
+            return new XyzTileResult(mapModel, x, y, z);
+        }
+
+        [HttpGet]
+        [Route("identify")]
+        public IHttpActionResult Identify(double x, double y, int z)
+        {
+            ShapefileLayer layer = new ShapefileLayer(HttpContext.Current.Server.MapPath("~/App_Data/world-bound-900913.shp"));
+            Feature feature = layer.Identify(new GeoCoordinate(x, y), new ScaleLevels()[z].Scale, GeoUnit.Meter).FirstOrDefault();
+            if (feature != null)
+            {
+                return Json(feature);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+    }
+}
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using SlimGis.MapKit.Geometries;
+using SlimGis.MapKit.Layers;
+using SlimGis.MapKit.Utilities;
+
+namespace geovisual
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
